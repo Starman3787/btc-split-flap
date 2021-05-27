a23343a4b401e966d0e6d4248c2f632b4efea6c6
Starman <30315137+Starman3787@users.noreply.github.com>
2021-05-27 13:56:10 +0100

    create delay function

---
+++ software_v2/delay.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include "./timers/systick/systick.h"
+
+void delay_ms(uint16_t milliseconds)
+{
+    // milliseconds = 25UL;
+    uint64_t waitUntilTicks = ticks + milliseconds;
+    uint64_t currentTicks = ticks;
+    do
+    {
+        currentTicks = ticks;
+    } while (currentTicks < waitUntilTicks);
+}
