0c94aaf6c52d6c64ecd9b5d4ce408015b4492add
Starman <30315137+Starman3787@users.noreply.github.com>
2021-05-27 16:36:24 +0100

    only spin motors if sensor 1 detects a magnet

--- software_v2/main.c
+++ software_v2/main.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include "./timers/systick/systick.h"
 volatile uint64_t ticks = 0;
 
@@ -8,11 +10,17 @@ void main(void)
     init_systick();
     while (1)
     {
-        delay_ms(25);
-        write_motor(0);
-        write_motor(1);
-        write_motor(2);
-        write_motor(3);
-        write_motor(4);
+
+        bool sensor1 = read_hall_effect_sensor(0);
+        if (sensor1)
+        {
+            delay_ms(25);
+            write_motor(0);
+            write_motor(1);
+            write_motor(2);
+            write_motor(3);
+            write_motor(4);
+        }
+        
     }
 }
