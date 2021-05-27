508e495b876440cc9f9c4c1ee1cb00a948de152c
Starman <30315137+Starman3787@users.noreply.github.com>
2021-05-27 18:57:28 +0100

    properly unset bits

--- software_v2/drivers/hall_effect_sensor/hall_effect_sensor.c
+++ software_v2/drivers/hall_effect_sensor/hall_effect_sensor.c
@@ -15,6 +15,7 @@ void init_hall_effect_sensor(void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
     GPIOE->MODER &= (0b11 << (hall_effect_sensor_pins[0] * 2));
+    GPIOE->MODER &= ~(0b11 << (hall_effect_sensor_pins[0] * 2));
 }
 
 bool read_hall_effect_sensor(uint16_t sensor)
