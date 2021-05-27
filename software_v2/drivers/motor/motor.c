61d4b4f826886e9a637e15dcd7681a2a4f3de9b1
Starman <30315137+Starman3787@users.noreply.github.com>
2021-05-27 14:13:23 +0100

    add function to toggle motors on and off

--- software_v2/drivers/motor/motor.c
+++ software_v2/drivers/motor/motor.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 /**
- * Pin  - Sensor
+ * Pin  - Motor
  * 
  * PF7  - 0
  * PF8  - 1
@@ -18,12 +18,27 @@ uint8_t motor_pins[] = {
     10,
     15};
 
+/**
+ * Pin  - Motor
+ * 
+ * PD0  - 0
+ * PD1  - 1
+ * PD2  - 2
+ * PD3  - 3
+ * PD4  - 4
+ * 
+ */
 uint8_t motor_enable_pins[] = {
-    };
+    0,
+    1,
+    2,
+    3,
+    4};
 
 void init_motor(void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
     GPIOF->MODER |= (0b01 << (motor_pins[0] * 2));
     GPIOF->MODER |= (0b01 << (motor_pins[1] * 2));
     GPIOF->MODER |= (0b01 << (motor_pins[2] * 2));
@@ -38,5 +53,5 @@ void write_motor(uint16_t motor)
 
 void toggle_motor(uint16_t motor)
 {
-    
+    GPIOD->ODR ^= (0b1 << motor_enable_pins[motor]);
 }
