0176837a790a4e8a230d286b89865b269e82dec7
Starman <30315137+Starman3787@users.noreply.github.com>
2021-05-29 21:13:31 +0100

    add commenting

--- software_v2/timers/systick/systick.h
+++ software_v2/timers/systick/systick.h
@@ -1,3 +1,13 @@
+/**
+ * @file systick.h
+ * @author Starman
+ * @brief SysTick header
+ * @version 0.1
+ * @date 2021-05-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <stdint.h>
 
 void SysTick_Handler(void);
