2641de6f402cafefa8bbffc4962fd74a2e1f6660
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-09 21:35:54 +0100

    add wifi init and test command

--- software_v2/drivers/wifi/wifi.c
+++ software_v2/drivers/wifi/wifi.c
@@ -1,11 +1,30 @@
+#include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
+
+bool send_test_command(char *command)
+{
+    write_full_uart(command);
+    if (read_full_uart_and_expect(strcat(command, "\r\n")) == false)
+        return false;
+    if (read_full_uart_and_expect("\r\r\n\r\n") == false)
+        return false;
+    if (read_full_uart_and_expect("OK\r\n") == false)
+        return false;
+    return true;
+}
+
+bool send_cip_start_command(char *protocol, char *host, uint8_t port)
+{
+    
+}
 
 void init_wifi(void)
 {
-    char current[] = "AT";
-    for (uint16_t i = 0; i < sizeof current; i++)
-        write_uart(current[i]);
-
-    char read_value = read_uart();
-    // breakpoint here
+    write_led(1, true);
+    delay_ms(5000);
+    if (send_test_command("AT") == false)
+        write_led(2, true);
+    else
+        write_led(0, true);
 }
