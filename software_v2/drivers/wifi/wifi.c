78dcc8c38e6c6b11bd5eaaac5d26fb3d048ae79b
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-10 01:48:05 +0100

    add wifi functions

--- software_v2/drivers/wifi/wifi.c
+++ software_v2/drivers/wifi/wifi.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
+#include "./wifi.h"
 
 bool send_test_command(char *command)
 {
@@ -14,17 +15,57 @@ bool send_test_command(char *command)
     return true;
 }
 
-bool send_cip_start_command(char *protocol, char *host, uint8_t port)
+bool send_cip_start_command(char *protocol, char *host, char *port)
 {
-    
+    char command[strlen("AT+CIPSTART=\"") + strlen(protocol) + strlen("\",\"") + strlen(host) + strlen("\",") + strlen(port) + 1];
+    command[0] = '\0';
+    strcat(command, "AT+CIPSTART=\"");
+    strcat(command, protocol);
+    strcat(command, "\",\"");
+    strcat(command, host);
+    strcat(command, "\",");
+    strcat(command, port);
+    write_full_uart(command);
+    if (read_full_uart_and_expect(strcat(command, "\r\r\n")) == false)
+        return false;
+    if (read_full_uart_and_expect("CONNECT\r\n") == false)
+        return false;
+    if (read_full_uart_and_expect("\r\n") == false)
+        return false;
+    if (read_full_uart_and_expect("OK\r\n") == false)
+        return false;
+    return true;
+}
+
+bool send_cip_send_command(char *size, char *httpRequest)
+{
+    char command[strlen("AT+CIPSEND=") + strlen(size) + 1];
+    command[0] = '\0';
+    strcat(command, "AT+CIPSEND=");
+    strcat(command, size);
+    write_full_uart(command);
+    if (read_full_uart_and_expect(strcat(command, "\r\r\n")) == false)
+        return false;
+    if (read_full_uart_and_expect("\r\n") == false)
+        return false;
+    if (read_full_uart_and_expect("OK\r\n") == false)
+        return false;
+    if (read_full_uart_and_expect(">") == false)
+        return false;
+    write_full_uart(httpRequest);
+    return true;
 }
 
 void init_wifi(void)
 {
     write_led(1, true);
     delay_ms(5000);
-    if (send_test_command("AT") == false)
+    if (send_test_command(TEST) == false)
         write_led(2, true);
     else
         write_led(0, true);
+    if (send_cip_start_command("TCP", "rest.coinapi.io", "80") == false)
+        write_led(2, true);
+    if (send_cip_send_command("143", HTTP_REQUEST) == false)
+        write_led(2, true);
 }
