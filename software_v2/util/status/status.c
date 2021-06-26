6989747422787db3d3013c397646b3a64dcd7c85
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-27 00:25:32 +0100

    add standard status functions

---
+++ software_v2/util/status/status.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include "./status.h"
+
+void status_error(bool status)
+{
+    write_led(ERROR_PIN, status);
+}
+
+void status_loading(bool status)
+{
+    write_led(LOADING_PIN, status);
+}
+
+void status_ok(bool status)
+{
+    write_led(OK_PIN, status);
+}
+
+void status_loading_flash(void)
+{
+    toggle_led(LOADING_PIN);
+}
---
+++ software_v2/util/status/status.h
@@ -0,0 +1,3 @@
+#define ERROR_PIN       (2U)
+#define LOADING_PIN     (1U)
+#define OK_PIN          (0U)
