42428530352d9faf89242ea04a37f68593ebc786
Starman <30315137+Starman3787@users.noreply.github.com>
2021-09-11 21:09:31 +0100

    use unsigned postfix

--- software_v2/timers/systick/systick.c
+++ software_v2/timers/systick/systick.c
@@ -6,7 +6,7 @@ void init_systick(void)
 {
     SysTick->CTRL |= (0b1 << 1); /* enables the SysTick interrupt */
     SysTick->CTRL |= (0b1 << 2); /* sets the clock source to the processor clock */
-    SysTick->LOAD = 18749; /* sets the reload value to 18749, since the clock runs at 18.75 MHz */
-    SysTick->VAL = 0; /* set the current value to 0 */
+    SysTick->LOAD = 18749U; /* sets the reload value to 18749, since the clock runs at 18.75 MHz */
+    SysTick->VAL = 0U; /* set the current value to 0 */
     SysTick->CTRL |= (0b1 << 0); /* enables the counter */
 }
