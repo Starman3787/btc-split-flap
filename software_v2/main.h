741909f8e09084e7c36da089c8b37bd4a07615f2
Starman <30315137+Starman3787@users.noreply.github.com>
2022-06-02 13:46:25 +0100

    Update main.h

--- software_v2/main.h
+++ software_v2/main.h
@@ -1,13 +1,18 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #define MODULE_COUNT        (5)
 
-#define TEST            ("AT")
-#define REQUEST         ("GET /v1/exchangerate/BTC/GBP HTTP/1.0\r\nHost: rest.coinapi.io\r\nX-CoinAPI-Key: 4354354-NOT-A-REAL-API-KEY-432254543\r\nAccept: application/json\r\n\r\n")
-#define REQUEST_SIZE    ("143")
-#define HOST            ("rest.coinapi.io")
-#define PORT            ("80")
-#define PROTOCOL        ("TCP")
+enum modeOptions {
+    btc,
+    custom
+};
+
+extern uint8_t currentMode;
+extern char modeCustomText[MODULE_COUNT + 1];
+extern bool newModeUpdated;
 
 #endif
