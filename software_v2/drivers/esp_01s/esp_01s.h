edd0a867afdc549d4cd22da9306de178ad14bbc6
Starman <30315137+Starman3787@users.noreply.github.com>
2022-06-02 13:50:36 +0100

    Update esp_01s.h

--- software_v2/drivers/esp_01s/esp_01s.h
+++ software_v2/drivers/esp_01s/esp_01s.h
@@ -5,7 +5,7 @@
 #include "util/http/http.h"
 
 int8_t init_esp_01s(void);
-int8_t make_http_request(Http *response, char *jsonProperty, char *protocol, char *host, char *port, char *size, char *httpRequest);
+int8_t make_http_request(Http *response, char *jsonProperty);
 int8_t response_parser(Http *response, char *jsonProperty);
 
 #endif
