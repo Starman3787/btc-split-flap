6b79f78167e694a05c68f2a6ad4497bb9b5dded3
Starman <30315137+Starman3787@users.noreply.github.com>
2022-06-02 13:50:14 +0100

    improve handling of uart

--- software_v2/drivers/uart/uart.c
+++ software_v2/drivers/uart/uart.c
@@ -1,12 +1,16 @@
 #include <stdint.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "headers/stm32f767xx.h"
 #include "drivers/uart/uart.h"
 #include "timers/systick/systick.h"
+#include "main.h"
+#include "drivers/split_flap/split_flap.h"
 
 /**
  * Pin  - Motor
- * 
+ *
  * PC10 - UART4_TX
  * PC11 - UART4_RX
  * PB9  - UART5_TX
@@ -21,7 +25,7 @@ uint8_t uart_pins[4] = {
 
 /**
  * @brief Initialises the UART pins
- * 
+ *
  */
 int8_t init_uart(void)
 {
@@ -44,19 +48,124 @@ int8_t init_uart(void)
                     (0b10 << (uart_pins[3] * 2U));
 
     // calculate and set the baud rate
-    uint16_t uartdiv = CORE_CLOCK / BAUD_RATE;
-    UART4->BRR = uartdiv;
-    UART5->BRR = uartdiv;
+    UART4->BRR = (CORE_CLOCK / BAUD_RATE);
+    UART5->BRR = (CORE_CLOCK / BAUD_RATE_DEBUG);
+
+    NVIC_EnableIRQ(UART4_IRQn);
 
     // enable the RX, TX and then enable the UART
     UART4->CR1 |= USART_CR1_RE |
                   USART_CR1_TE |
+                  USART_CR1_RXNEIE |
                   USART_CR1_UE;
     UART5->CR1 |= USART_CR1_RE |
                   USART_CR1_TE |
                   USART_CR1_UE;
 
     return 0;
+}
+
+void UART4_IRQHandler(void)
+{
+    irq_uart_char = UART4->RDR;
+    irq_uart_char_read = false;
+}
+
+void check_incoming_config(void)
+{
+    UART4_Interrupts_Disable();
+    if (find_pattern("ONFIG:", 6, 100) == 0)
+    {
+        char configSetting = read_uart(100);
+        read_uart(100);
+        read_uart(100);
+        switch (configSetting)
+        {
+        case '0':
+        {
+            // off
+            break;
+        }
+        case '1':
+        {
+            // on
+            break;
+        }
+        case '2':
+        {
+            // standby
+            // char rawTime[5];
+            // rawTime[0] = read_uart(100);
+            // rawTime[1] = read_uart(100);
+            // read_uart(100);
+            // rawTime[2] = read_uart(100);
+            // rawTime[3] = read_uart(100);
+            // rawTime[4] = '\0';
+            // read_uart(100);
+            // read_uart(100);
+            break;
+        }
+        }
+    }
+    UART4_Interrupts_Enable();
+}
+
+void check_incoming_mode(void)
+{
+    UART4_Interrupts_Disable();
+    if (find_pattern("ODE:", 4, 100) == 0)
+    {
+        char configSetting = read_uart(100);
+        printf("CONFIG: %c\n", configSetting);
+        read_uart(100);
+        read_uart(100);
+        switch (configSetting)
+        {
+        case '0':
+        {
+            // btc
+            currentMode = btc;
+            newModeUpdated = false;
+            break;
+        }
+        case '1':
+        {
+            // custom
+            int16_t customLength = 0;
+            char uartChar = read_uart(50);
+            while (uartChar != ':')
+            {
+                customLength *= 10;
+                customLength += (uartChar - '0');
+                uartChar = read_uart(50);
+            }
+            for (uint8_t i = 0; i < customLength; i++)
+            {
+                char currentCustomChar = read_uart(50);
+                if (isalnum(currentCustomChar) != 0 && i < MODULE_COUNT)
+                {
+                    modeCustomText[i] = currentCustomChar;
+                    modeCustomText[i + 1] = '\0';
+                }
+            }
+            printf("CUSTOM TEXT: %s\n", modeCustomText);
+            currentMode = custom;
+            newModeUpdated = false;
+            break;
+        }
+        }
+    }
+    UART4_Interrupts_Enable();
+}
+
+void UART4_Interrupts_Enable(void)
+{
+    UART4->CR1 |= USART_CR1_RXNEIE;
+}
+
+void UART4_Interrupts_Disable(void)
+{
+    UART4->CR1 &= ~(USART_CR1_RXNEIE);
 }
 
 // writes a single character to the UART
@@ -148,8 +257,11 @@ int8_t read_full_uart_and_expect(char *message, uint16_t timeout)
 int8_t find_pattern(char *pattern, uint16_t patternLength, uint16_t timeout)
 {
     uint8_t matchingChars = 0;
+    uint64_t readUntil = ticks + (uint64_t)(timeout * patternLength * 5);
     while (matchingChars != patternLength)
     {
+        if (readUntil < ticks)
+            return -1;
         char currentValue = read_uart(timeout);
         if (currentValue == *(pattern + matchingChars))
             matchingChars++;
--- software_v2/drivers/uart/uart.h
+++ software_v2/drivers/uart/uart.h
@@ -2,11 +2,15 @@
 #define DRIVERS_UART_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 extern uint8_t uart_pins[4];
+extern char irq_uart_char;
+extern bool irq_uart_char_read;
 
 #define CORE_CLOCK      (16000000U)
-#define BAUD_RATE       (115200U)
+#define BAUD_RATE       (9600U)
+#define BAUD_RATE_DEBUG (115200U)
 
 int8_t init_uart(void);
 void print(char byte);
@@ -16,5 +20,10 @@ char read_uart(uint16_t timeout);
 void write_full_uart(char *message);
 int8_t read_full_uart_and_expect(char *message, uint16_t timeout);
 int8_t find_pattern(char *pattern, uint16_t patternLength, uint16_t timeout);
+void UART4_IRQHandler(void);
+void UART4_Interrupts_Enable(void);
+void UART4_Interrupts_Disable(void);
+void check_incoming_config(void);
+void check_incoming_mode(void);
 
 #endif
